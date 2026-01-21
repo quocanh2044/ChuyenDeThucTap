3944ae3df322168f2672ac57380f89dd116cb22a
Quốc Anh <quocanh@MacBook-Air-cua-Quoc.local>
2026-01-21 16:09:10 +0700

    replace bcrypt with bcryptjs for render deploy

--- backend10/controllers/auth.controller.js
+++ backend10/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 import db from "../config/db.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 const JWT_SECRET = process.env.JWT_SECRET || "SECRET_KEY";
